fff810b6f3b21caee811a11f2c6f1927035a260e
debiris <debora.hsf2000@gmail.com>
2024-06-25 22:36:09 -0300

    cors

--- api/app.js
+++ api/app.js
@@ -6,27 +6,32 @@ const authMiddleware = require('./middleware/auth');
 const User = require('./models/User');
 const authRoutes = require('./routes/auth');
 
-
 dotenv.config();
 const app = express();
 
-app.use(cors());
+// Configuração do CORS para permitir requisições do Vercel
+const corsOptions = {
+  origin: 'https://mangue-runner-b7bbj31uc-debiris-projects.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Conectar ao MongoDB
 mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.log(err));
-
-
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
 
 app.use('/api/auth', authRoutes);
 
 // Rota protegida de exemplo
 app.get('/api/protected', authMiddleware, (req, res) => {
-    res.send('Esta é uma rota protegida');
+  res.send('Esta é uma rota protegida');
 });
 
 const PORT = process.env.PORT || 5000;
